fa5a59510ff33db72d7dd13744124efebf15ce0e
Flor <florenciasack123@gmail.com>
2026-06-22 11:58:41 -0300

    reworked reactnativebridge and reactconnection to use Unity WebGL

--- Assets/Script/React Native/React_Connection.cs
+++ Assets/Script/React Native/React_Connection.cs
@@ -1,64 +1,24 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class React_Connection : MonoBehaviour
 {
-    public Nivel_1 nivel1;
-
-    void Start()
-    {
-        Debug.Log("React_Connection arrancó");
-
-        Debug.Log("nivel1 = " + nivel1);
-
-        if (nivel1 == null)
-        {
-            Debug.LogError("Nivel_1 no asignado en React_Connection");
-            return;
-        }
-
-        Debug.Log("Nivel_1 encontrado correctamente");
-
-        nivel1.OnLevelCompleted += SendResultToReact;
-    }
-
-    void OnDestroy()
-    {
-        if (nivel1 != null)
-        {
-            nivel1.OnLevelCompleted -= SendResultToReact;
-        }
-    }
+    [DllImport("__Internal")]
+    private static extern void SendToReactNative(string json);
 
     public void SendResultToReact()
     {
-        ResultData result = new ResultData
+        string json = JsonUtility.ToJson(new ResultData
         {
             levelId = 1,
-            completed = true,
+            completed = true
+        });
 
-            phase1Time = nivel1.Phase1Time,
-            phase2Time = nivel1.Phase2Time,
-            averageTime = nivel1.AverageTime,
-
-            phase1Stars = nivel1.Phase1Stars,
-            phase2Stars = nivel1.Phase2Stars
-        };
-
-        string json = JsonUtility.ToJson(result);
-
-        Debug.Log("Enviando a React Native:");
+#if UNITY_WEBGL && !UNITY_EDITOR
+        SendToReactNative(json);
+#else
         Debug.Log(json);
-
-#if UNITY_ANDROID
-        using (AndroidJavaClass jc =
-            new AndroidJavaClass(
-                "com.azesmwayreactnativeunity.ReactNativeUnityViewManager"))
-        {
-            jc.CallStatic(
-                "sendMessageToMobileApp",
-                json
-            );
-        }
 #endif
     }
 }
