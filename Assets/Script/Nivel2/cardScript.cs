86122abee4ae13fcdf4e380ee45eeaa96aaac6c4
Julieta Usoz <48799592@est.ort.edu.ar>
2026-06-30 22:43:05 -0300

    script & carpetas

--- Assets/Script/Nivel2/cardScript.cs
+++ Assets/Script/Nivel2/cardScript.cs
@@ -1,129 +1,129 @@
-﻿//using UnityEngine;
-//using UnityEngine.UI;
-//using static UnityEngine.UI.Image;
-//using static System.Net.Mime.MediaTypeNames;
-//using UnityEngine.SceneManagement;
-//using TMPro;
-////using static System.Net.Mime.MediaTypeNames.Image;
-//using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UIImage = UnityEngine.UI.Image;
+using static System.Net.Mime.MediaTypeNames;
+using UnityEngine.SceneManagement;
+using TMPro;
+//using static System.Net.Mime.MediaTypeNames.Image;
+using System.Collections;
 
 
-//public class cardScript : MonoBehaviour
-//{
-//    [Header("Componentes")]
+public class cardScript : MonoBehaviour
+{
+    [Header("Componentes")]
 
-//    //[SerializeField] private Image cardImage; //ANTES ERA IMG
+    [SerializeField]
+    private UIImage cardImage;
+    [SerializeField] private Sprite backSprite;
 
-//    [SerializeField] private Sprite backSprite;
+    [Header("Datos")]
 
-//    [Header("Datos")]
+    public MaterialDialisis material;
 
-//    public MaterialDialisis material;
+    private Level_2 gameManager;
 
-//    private Level_2 gameManager;
+    private bool descubierta = false;
 
-//    private bool descubierta = false;
+    private bool encontrada = false;
 
-//    private bool encontrada = false;
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<Level_2>();
+    }
 
-//    private void Awake()
-//    {
-//        gameManager = FindObjectOfType<Level_2>();
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    private void Start()
+    {
+        OcultarCarta();
+    }
 
-//    private void Start()
-//    {
-//        OcultarCarta();
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public void Configurar(MaterialDialisis nuevoMaterial)
+    {
+        material = nuevoMaterial;
 
-//    public void Configurar(MaterialDialisis nuevoMaterial)
-//    {
-//        material = nuevoMaterial;
+        cardImage.sprite = backSprite;
 
-//        cardImage.sprite = backSprite;
+        descubierta = false;
 
-//        descubierta = false;
+        encontrada = false;
+    }
 
-//        encontrada = false;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public void ClickCarta()
+    {
+        if (encontrada)
+            return;
 
-//    public void ClickCarta()
-//    {
-//        if (encontrada)
-//            return;
+        if (descubierta)
+            return;
 
-//        if (descubierta)
-//            return;
+        if (gameManager.PuedeSeleccionar() == false)
+            return;
 
-//        if (gameManager.PuedeSeleccionar() == false)
-//            return;
+        MostrarCarta();
 
-//        MostrarCarta();
+        gameManager.SeleccionarCarta(this);
+    }
 
-//        gameManager.SeleccionarCarta(this);
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public void MostrarCarta()
+    {
+        descubierta = true;
 
-//    public void MostrarCarta()
-//    {
-//        descubierta = true;
+        cardImage.sprite = material.imagen;
+    }
 
-//        cardImage.sprite = material.imagen;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public void OcultarCarta()
+    {
+        if (encontrada)
+            return;
 
-//    public void OcultarCarta()
-//    {
-//        if (encontrada)
-//            return;
+        descubierta = false;
 
-//        descubierta = false;
+        cardImage.sprite = backSprite;
+    }
 
-//        cardImage.sprite = backSprite;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public void MarcarComoEncontrada()
+    {
+        encontrada = true;
+    }
 
-//    public void MarcarComoEncontrada()
-//    {
-//        encontrada = true;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public bool EstaDescubierta()
+    {
+        return descubierta;
+    }
 
-//    public bool EstaDescubierta()
-//    {
-//        return descubierta;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public bool EstaEncontrada()
+    {
+        return encontrada;
+    }
 
-//    public bool EstaEncontrada()
-//    {
-//        return encontrada;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
+    public int ObtenerID()
+    {
+        return material.id;
+    }
 
-//    public int ObtenerID()
-//    {
-//        return material.id;
-//    }
+    //--------------------------------------------------
 
-//    //--------------------------------------------------
-
-//    public MaterialDialisis ObtenerMaterial()
-//    {
-//        return material;
-//    }
-//}
+    public MaterialDialisis ObtenerMaterial()
+    {
+        return material;
+    }
+}
