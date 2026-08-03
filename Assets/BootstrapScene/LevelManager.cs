3727e9c667c47172f4c17b814fb9dbe0ab8c2817
Android Studio <devandroid@ort>
2026-08-03 12:16:11 -0300

    principios bri

--- Assets/BootstrapScene/LevelManager.cs
+++ Assets/BootstrapScene/LevelManager.cs
@@ -4,8 +4,10 @@ public class LevelManager : MonoBehaviour
 {
     private Level_1 level1Manager;
     private Level_2 level2Manager;
+    private Level_4 level4Manager;
     [SerializeField] private GameObject nivel1;
     [SerializeField] private GameObject nivel2;
+    [SerializeField] private GameObject nivel4;
     [SerializeField] private int debugLevel = 2;
 
     private void OnEnable()
@@ -21,15 +23,17 @@ public class LevelManager : MonoBehaviour
     {
         level1Manager = nivel1.GetComponentInChildren<Level_1>(true);
         level2Manager = nivel2.GetComponentInChildren<Level_2>(true);
+        level4Manager = nivel4.GetComponentInChildren<Level_4>(true);
     }
 
     private void Start()
     {
         nivel1.SetActive(false);
         nivel2.SetActive(false);
+        nivel4.SetActive(false);
 #if UNITY_EDITOR
         Debug.Log($"[DEBUG] Iniciando automáticamente el nivel {1}");
-        StartLevel(1);
+        StartLevel(4);
 #else
         ReactConnection react = FindFirstObjectByType<ReactConnection>();
         react.Log("Iniciando nivel");
@@ -61,6 +65,7 @@ public class LevelManager : MonoBehaviour
     {
         nivel1.SetActive(false);
         nivel2.SetActive(false);
+        nivel4.SetActive(false);
 
         switch (level)
         {
@@ -74,6 +79,10 @@ public class LevelManager : MonoBehaviour
                 nivel2.SetActive(true);
                 level2Manager.StartLevel();
 
+                break;
+            case 4:
+                nivel4.SetActive(true);
+                level4Manager.StartLevel();
                 break;
         }
     }
---
+++ Assets/Script/Nivel4/Level_4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartLevel()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
