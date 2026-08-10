c8b164ece98ed768ec1b1b22e86bbe779af278d2
BRISA JULIETA MONASTIRIDIS VOROTNICOFF <49192786@ort>
2026-08-10 12:16:43 -0300

    empezando parte 2 nivel 4

--- Assets/Script/Nivel4/Bolsa.cs
+++ Assets/Script/Nivel4/Bolsa.cs
@@ -2,85 +2,43 @@ using UnityEngine;
 
 public class Bolsa : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
+    private bool bolsaAplicada = false;
+    public GameObject palo;
+    public Sprite palobolsas;
+    public paloRenderer manosRenderer;
 
-    public Sprite bolsa1;
-    public Sprite bolsa2;
-    public Sprite bolsa3;
-    public Sprite bolsa4;
 
-    private int bolsaActual = 1;
-
-    void Start()
+    void OnMouseDown()
     {
-        spriteRenderer.sprite = bolsa1;
-    }
+        agarrado = true;
 
+    }
+    void OnMouseUp()
+    {
+        agarrado = false;
+        float distancia = Vector2.Distance(transform.position, palo.transform.position);
+
+        if (distancia < 1.5f)
+        {
+            paloRenderer.sprite = palobolsas;
+            bolsaAplicada = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
     void Update()
     {
-
-    }
-
-    public void Tick()
-    {
-        if (bolsaActual == 4)
+        if (agarrado)
         {
-            Debug.Log("GANASTE");
+            Vector3 posicion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            posicion.z = 0;
+            transform.position = posicion;
+
         }
-        else
+        if (bolsaAplicada && Input.GetMouseButton(0))
         {
-            Reiniciar();
+            palo.sprite = palobolsas;
+            
         }
-    }
-
-    public void Cruz()
-    {
-        Debug.Log("Entró a Cruz");
-        Debug.Log("Bolsa actual: " + bolsaActual);
-
-        if (bolsaActual == 4)
-        {
-            Debug.Log("Era la bolsa buena");
-            Reiniciar();
-        }
-        else
-        {
-            Debug.Log("Pasa a la siguiente bolsa");
-            SiguienteBolsa();
-        }
-    }
-
-    void SiguienteBolsa()
-    {
-        bolsaActual++;
-
-        Debug.Log("Ahora la bolsa es: " + bolsaActual);
-
-        if (bolsaActual == 2)
-        {
-            Debug.Log("Cambiando a bolsa2");
-            spriteRenderer.sprite = bolsa2;
-            Debug.Log("Sprite actual: " + spriteRenderer.sprite.name);
-        }
-        else if (bolsaActual == 3)
-        {
-            Debug.Log("Cambiando a bolsa3");
-            spriteRenderer.sprite = bolsa3;
-            Debug.Log("Sprite actual: " + spriteRenderer.sprite.name);
-        }
-        else if (bolsaActual == 4)
-        {
-            Debug.Log("Cambiando a bolsa4");
-            spriteRenderer.sprite = bolsa4;
-            Debug.Log("Sprite actual: " + spriteRenderer.sprite.name);
-        }
-    }
-
-    void Reiniciar()
-    {
-        Debug.Log("INTENTELO DE NUEVO");
-
-        bolsaActual = 1;
-        spriteRenderer.sprite = bolsa1;
     }
 }
