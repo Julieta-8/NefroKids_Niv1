2f28e1af93c7fd574a09250b20cfe71d2a2351b0
JulietaUsoz8 <julieta.usoz@gmail.com>
2026-09-07 12:10:33 -0300

    level 5 scene created

---
+++ Assets/Script/Nivel5/Level_5.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_5 : MonoBehaviour
+{
+    /*Va constar de un nivel donde se enfocará en la perilla y como moverla, + como cerrar el programa
+     
+     -hacer zoom a la perilla
+    -Arrastrar la perilla cerrada para desbloquearla
+    -En esta parte se debe lograr que la perilla pueda rotar en vez de moverla, que mantenga su psociones, pero cuando el usuario la toca debe solo rotar
+    y debe tener 4 psoiciones con sensores que detecten cuando la perilla los apunta (esto podria hacerse con flechas como botones, una indicando a la derecha y otra a la izquierda, donde invocan una animacion del a perilla girando de una posicion a la otra, en vez de tener que adivinar dond eel ususario psociono la perilla)
+    1)drenaje pos 1 (riku va a mencionar para que sirve cada una cuando cosnigue la correcta) --> cuando el jugador mueve la perilla deberia haber un boton que diga "verificar" para saber si esta realmente seguro si es esa psoiciones, en caso de ser incorrecto aparecerá riku con su texto
+    2)el nene debera guiar el flujo (va a ser el cable con una pantalla atras que representara el liquido, debera arrastrarla para que se drene)
+    3) cambio de posiciones, ahora la bolsa debee recibir, cambio de perilla a pos 2(riku vuelve a explicar para que sirve)
+    4)vuelve a seguir el liquido
+    5)lavado completado
+    6)infusion: pos 3
+    7)cierre: pos 4
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
